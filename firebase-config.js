eeed3e862e1e4136e66ca62521f2cf486cf1f087
waineetsha <wai200423@gmail.com>
2026-04-19 00:18:30 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// ════════════════════════════════════════════════
+// ★ ここにあなたのFirebase設定を貼り付けてください ★
+// ════════════════════════════════════════════════
+const firebaseConfig = {
+  apiKey: "AIzaSyAO5TuFOawHyQAA5hkKAPO5s8JdUZW1SfI",
+  authDomain: "who-ate--the-cheese.firebaseapp.com",
+  projectId: "who-ate--the-cheese",
+  storageBucket: "who-ate--the-cheese.firebasestorage.app",
+  messagingSenderId: "135374886454",
+  appId: "1:135374886454:web:4c90390dcc64763b872a97",
+  measurementId: "G-W6CJMF0NPX"
+};
